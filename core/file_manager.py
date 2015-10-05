672ae85e609e252e4c60ddda79d8811904bdf20e
MarcoQin <qyyfy2009@163.com>
2015-10-05 17:40:46 +0800

    delete file

--- core/file_manager.py
+++ core/file_manager.py
@@ -8,8 +8,21 @@ class FileManager(object):
         self.db = DB()
 
     def add_files(self, files):
+        sql = 'insert into songs (name, path) values '
         for file in files:
             file_name = file.rsplit('/')[-1]
-            sql = 'insert into songs (name, path) values ("{0}", "{1}")'.format(file_name, file)
-            self.db.execute(sql)
+            sql += '("{0}", "{1}"),'.format(file_name, file)
+        sql = sql[:-1]
+        self.db.execute(sql)
 
+    def del_files(self, ids):
+        """
+        require: ids: list, tuple, set
+        """
+        if not ids:
+            return
+        query = str(tuple(ids))
+        if len(ids)<2:
+            query = query.replace(',', '')
+        sql = 'delete from songs where id in {0}'.format(query)
+        self.db.execute(sql)
