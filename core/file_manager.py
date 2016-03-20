700653e3d1c60e0994e712a28904112543f5e389
MarcoQin <qyyfy2009@163.com>
2016-03-20 15:20:51 +0800

    add file change

--- core/file_manager.py
+++ core/file_manager.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from db import DB
+from CPlayer import cp_get_metadata_py
 
 
 class FileManager(object):
@@ -8,10 +9,14 @@ class FileManager(object):
         self.db = DB()
 
     def add_files(self, files):
-        sql = 'insert into songs (name, path) values '
+        #  sql = 'insert into songs (name, path) values '
+        sql = 'insert into songs (name, path, title, album, artist, genre, track, date) values'
         for file in files:
+            info = cp_get_metadata_py(file.decode('utf-8').encode('utf-8'))
             file_name = file.rsplit('/')[-1]
-            sql += '("{0}", "{1}"),'.format(file_name, file)
+            info['name'] = file_name
+            info['path'] = file
+            sql += '("{name}", "{path}", "{title}", "{album}", "{artist}", "{genre}", "{track}", "{date}"),'.format(**info)
         sql = sql[:-1]
         self.db.execute(sql)
 
